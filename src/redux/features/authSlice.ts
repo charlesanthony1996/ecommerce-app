1905382626ef09a26ad0c297a6ca7a8f828ae174
charlesanthony1996 <charlesanthony296@gmail.com>
2024-10-17 13:32:41 +0200

    authslice.ts file

---
+++ src/redux/features/authSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { AuthSlice } from "../../models/AuthSlice"
+
+interface LoginProps {
+    username: string
+    password: string
+}
+
+const initialState: AuthSlice = {
+    isloggedIn:
+    localStorage.getItem("username") !== null &&
+    localStorage.getItem("username") !== undefined &&
+    localStorage.getItem('username') !== "",
+    modalOpen: false,
+    username: localStorage.getItem("username") ?? ""
+}
+
+export const authSlice = createSlice({
+    name: "authslice",
+    initialState,
+    reducers: {
+        updateModal: (state, action: PayloadAction<boolean>) => {
+            return { ...state, modalOpen: action.payload }
+        },
+        doLogin:(state, action: PayloadAction<LoginProps>) => {
+            if (action.payload.username === "atuny0" && action.payload.password === "9uQFF1Lh") {
+                localStorage.setItem("username", "atuny0")
+                return {
+                    ...state,
+                    username: "atuny0",
+                    modalOpen: false,
+                    isLoggedIn: true
+                }
+            } else {
+                return state
+            }
+        },
+        doLogout: (state) => {
+            localStorage.removeItem("username")
+            return { ...state, username: "", isLoggedIn: false}
+        },
+    },
+})
+
+export const { updateModal, doLogin, doLogout } = authSlice.actions
+export default authSlice.reducer
