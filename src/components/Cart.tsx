77a17d7d54f0e84c92f18adc2fee12bf5dff2ac6
charlesanthony1996 <charlesanthony296@gmail.com>
2024-10-22 07:55:15 +0200

    cart component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,9 @@
 import { FC, useState } from "react";
-import { RxCross1 } from "react-dom"
+import { RxCross1 } from "react-icons/rx"
 import { useAppDispatch, useAppSelector } from "../redux/hooks"
 import { emptyCart, setCartState } from "../redux/features/cartSlice"
 import CartRow from "./CartRow"
-import Toast from "react-hot-toast"
+import toast from "react-hot-toast"
 
 const Cart: FC = () => {
     const dispatch = useAppDispatch()
@@ -12,6 +12,56 @@ const Cart: FC = () => {
     const [checkout, setCheckout] = useState(false)
 
     const calculateTotal = () => {
-        
+        let total = 0
+        items.forEach((item) => {
+            if(item.quantity && item.discountPercentage) {
+                total += (item.price - (item.price * item.discountPercentage) / 100) * item.quantity
+            }
+        })
+        return total.toFixed(2)
+    }
+
+    const handleOrder = () => {
+        dispatch(setCartState(false))
+        dispatch(emptyCart())
+        setCheckout(false)
+        toast.success("your order has been confirmed", { duration: 3000})
+    }
+
+    if (isOpen) {
+        return (
+            <div>
+                <div>
+                    <h1>Checkout</h1>
+                    <p>Welcome to the checkout section</p>
+                    <div>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <h3></h3>
+                    </div>
+                    <div>
+                        <div>
+                            <img></img>
+                            <p>Your cart is empty</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h2></h2>
+                        <h2></h2>
+                    </div>
+                    <button>
+                        Checkout
+                    </button>
+                </div>
+            </div>
+        )
+
     }
 }
+
+export default Cart
