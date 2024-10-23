adb5d83f52ef89783ea18faabae75cdd2e46a96d
Charles Anthony <anthony@nu-it.at>
2024-10-23 09:10:24 +0200

    latest changes

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -11,13 +11,54 @@ const LoginModal: FC = () => {
     const [clicked, setClicked] = useState(false)
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-    
+    const dispatch = useAppDispatch()
+    const open = useAppSelector((state) => state.authReducer.modalOpen)
 
-    return (
-        <div>
+    const submitForm = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        dispatch(doLogin({ username, password}))
+    }
 
-        </div>
-    )
+    if(open) {
+        return (
+            <div className="">
+                <div className="relative border shadow rounded p-8">
+                    <RxCross1 className="absolute cursor-pointer right-5 top-5 hover:opacity-85">
+
+                    </RxCross1>
+                    { clicked ? (
+                        <>
+                        <div className="flex mb-2 space-x-2 justify-center items-center">
+                            <GiArchiveRegister></GiArchiveRegister>
+                            <h3 className="font-bold text-center text-xl">Register</h3>
+                            <GiArchiveRegister></GiArchiveRegister>
+                            <p className="">
+                                This is a hobby prohect for development purpose only.No well suited
+                                backend has been used here. Please use<b>charles</b> as username &
+                                <b>test123</b> as password
+                            </p>
+                        </div>
+                        </>
+                    ): (
+                        <>
+                        <div className="flex mb-2 space-x-2 justify-center items-center">
+                            <FaUnlock></FaUnlock>
+                            <h3 className="font-bold text-center text-2xl">Login</h3>
+                            <FaUnlock></FaUnlock>
+                        </div>
+                        <form>
+                            <div>
+                                <input>
+                                </input>
+                                <RiUser3Fill></RiUser3Fill>
+                            </div>
+                        </form>
+                        </>
+                    )}
+                </div>
+            </div>
+        )
+    }
 }
 
 
