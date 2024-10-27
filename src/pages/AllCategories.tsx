6becf6a7c930f4b1acfcc72f34643c0e1e49131b
Charles Anthony <anthony@nu-it.at>
2024-10-27 23:28:02 +0100

    all categories component

--- src/pages/AllCategories.tsx
+++ src/pages/AllCategories.tsx
@@ -1,23 +1,48 @@
-import { FC } from 'react'
-import { useAppSelector } from '../redux/hooks'
-import ProductList from "../components/ProductList"
+import { FC, useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../redux/hooks'
+import { addCategories } from '../redux/features/productSlice'
+import { Link } from 'react-router-dom'
 
-const WishList: FC = () => {
-    const wishlist = useAppSelector((state) => state.productReducer.wishlist)
-    return(
-        <div className="container mx-auto font-karla min-h-[83vh]">
-            {wishlist.length > 0 ? (
-                <ProductList title="Your wishlist" products={wishlist}></ProductList>
-            ): (
-                <div className="flex flex-col justify-center items-center p-8">
-                    <img src="/emptyCart.jpg" className="w-60" alt="empty"></img>
-                    <p className="text-center text-xl font-semibold my-2 dark:text-white">
-                        Your wishlist is empty
-                    </p>
-                </div>
-            )}
+const AllCategories: FC = () => {
+    const dispatch = useAppDispatch()
+
+    const allCategories = useAppSelector((state) => state.productReducer.categories)
+
+    useEffect(() => {
+        const fetchCategories = () => {
+            fetch("https://dummyjson.com/products/categories")
+            .then((res) => res.json())
+            .then((data) => {
+                dispatch(addCategories(data))
+            })
+        }
+        if (allCategories.length === 0) fetchCategories()
+    }, [addCategories, dispatch])
+
+    return (
+        <div className='container mx-auto min-h-[83vh] p-4 font-karla'>
+            <span className='text-lg dark:text-white'>Categories</span>
+            <div className='grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 gap-2 my-2'>
+                {allCategories && 
+                allCategories.map((category) => (
+                    <div
+                    key={category.slug}
+                    className="bg-gray-100 dark:bg-slate-600 dark:text-white px-4 py-4 font-karla mr-2 mb-2"
+                    >
+                        <div className="text-lg">{category.name}</div>
+                        <Link
+                        to={{ pathname: `/category/${category.slug}`}}
+                        className='hover:underline text-blue-500'
+                        >
+                            View products
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
 
-export default WishList
+
+export default AllCategories
+
