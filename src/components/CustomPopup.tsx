9c3f3738d7d1109ef29399b2a6993a5b20bc2939
charlesanthony1996 <charlesanthony296@gmail.com>
2024-10-29 20:58:56 +0100

    custom popup comp

--- src/components/CustomPopup.tsx
+++ src/components/CustomPopup.tsx
@@ -24,11 +24,18 @@ const CustomPopup: FC = () => {
 
     return (
         <div className="relative font-karla">
-            <div>
+            <div
+            className="inline-block cursor-pointer hover:opacity-85 dark:text-white"
+            onClick={handlePopup}
+            data-test="username-poup"
+            >
                 {username}
             </div>
             {isVisible && (
-                <div className="" data-test="popup-content-list">
+                <div 
+                className="absolute p-4 left-[-50px] w-40 z-50 mt-2 rounded-md shadow-2xl bg-white ring-1 transition-all ring-black ring-opacity-5 focus:outline-none dark:bg-slate-600 dark:text-white" 
+                data-test="popup-content-list"
+                >
                     <table>
                         <tbody>
                             <tr>
@@ -36,17 +43,38 @@ const CustomPopup: FC = () => {
                                     <MdOutlineAccountCircle></MdOutlineAccountCircle>
                                 </td>
                                 <td className="hover:underline cursor-pointer text-lg pl-2">
-                                    <Link to="/account" onClick={hidePopup}></Link>
+                                    <Link to="/account" onClick={hidePopup}>Account</Link>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="text-center">
+                                    <MdFavoriteBorder></MdFavoriteBorder>
+                                </td>
+                                <td
+                                className="hover:underline cursor-pointer text-lg pl-2"
+                                data-test="wishlist-container"
+                                >
+                                    <Link to="/wishlist" onClick={hidePopup}>Wishlist</Link>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="text-center">
+                                    <MdOutlineLogout></MdOutlineLogout>
+                                </td>
+                                <td
+                                className="hover:underline cursor-pointer text-lg pl-2"
+                                onClick={handleLogout}
+                                data-test="logout-btn"
+                                >
+                                    Logout
                                 </td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
-
             )}
         </div>
     )
-    
 }
 
 export default CustomPopup
