5c24f84230103dd17a1f154a9340f2213321032a
charlesanthony1996 <charlesanthony296@gmail.com>
2024-10-29 21:25:52 +0100

    product list comp styling changes

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -7,14 +7,13 @@ const ProductList: FC<{ title: string; products: Product[]}> = ({
     title,
     products
 }) => (
-    <div className="">
-        <div className="">
-            <h2 className="">{title}</h2>
+    <div className="container mt-8 mx-auto px-4 dark:bg-slate-800">
+        <div className="sm:flex items-center justify-between">
+            <h2 className="text-4xl font-medium font-lora dark:text-white">{title}</h2>
         </div>
-
-        <div 
-        className=""
-        data-test="">
+        <div
+        className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4"
+        data-test="product-list-container">
             {products?.map((product) => (
                 <ProductCard
                 key={product.id}
